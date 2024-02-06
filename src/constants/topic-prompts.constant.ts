24f246331c2c30e92239e614591a1723dff64d82
Anthony Nwobodo <anthonynwobodo7@gmail.com>
2024-02-06 21:11:27 +0100

    feat(openai): prompt mod

--- src/constants/topic-prompts.constant.ts
+++ src/constants/topic-prompts.constant.ts
@@ -5,24 +5,24 @@ const topicPrompts: TopicPromptType[] = [
     topic: 'Backend Engineering',
     userProfession: 'Backend Engineer',
     prompt:
-      "Craft a short tweet about Backend Engineering. Share a compelling insight, fact, or opinion in less than 250 characters. Don't enclose the content in quotation marks and don't use emojis and hashtags.",
+      "Craft a short tweet about Backend Engineering. Share a compelling insight, tip, fact, or opinion in less than 250 characters. Don't enclose the content in quotation marks and don't use emojis and hashtags.",
   },
   {
     topic: 'Database Administration',
     userProfession: 'Database Administrator',
     prompt:
-      "Craft a short tweet about Database Administration. Share a compelling insight, fact, or opinion in less than 250 characters. Don't enclose the content in quotation marks and don't use emojis and hashtags.",
+      "Craft a short tweet about Database Administration. Share a compelling insight, tip, fact, or opinion in less than 250 characters. Don't enclose the content in quotation marks and don't use emojis and hashtags.",
   },
   {
     topic: 'SQL',
     userProfession: 'Database Administrator',
     prompt:
-      "Craft a short tweet about SQL. Share a compelling insight, fact, or opinion in less than 250 characters. Don't enclose the content in quotation marks and don't use emojis and hashtags.",
+      "Craft a short tweet about SQL. Share a compelling insight, tip, fact, or opinion in less than 250 characters. Don't enclose the content in quotation marks and don't use emojis and hashtags.",
   },
   {
     topic: 'API Security',
     userProfession: 'NodeJs Backend Engineer',
-    prompt: `Craft a short tweet about API Security. Share a compelling insight, fact, or opinion in less than 250 characters. Don't enclose the content in quotation marks and don't use emojis and hashtags.`,
+    prompt: `Craft a short tweet about API Security. Share a compelling insight, tip, fact, or opinion in less than 250 characters. Don't enclose the content in quotation marks and don't use emojis and hashtags.`,
   },
 ];
 
